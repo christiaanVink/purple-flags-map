1f71745c61c3faf0e7c48b17dc52d5506e44d0ff
Christiaan Vink <c.vink@reply.de>
2021-03-22 12:14:02 +0100

    add tiles

---
+++ football.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="football" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="football.png" width="96" height="96"/>
+</tileset>
